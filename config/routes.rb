e814b111c88cdc2270a863c7911058231c62fc3f
Alex Kutzke <alexkutzke@gmail.com>
2017-02-09 13:54:06 +0000

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :lists do
+    resources :tasks, only: [:new, :create, :destroy] do
+      get "done", on: :member
+    end
+  end
+
+  devise_for :users
+
+  root 'lists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
